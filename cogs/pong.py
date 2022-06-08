e22f5c32e9b6a1046c6bd777b435918675b9a3d3
Matty <matty258987@gmail.com>
2022-06-08 23:52:31 +0100

    pingpong

---
+++ cogs/pong.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+
+class Pong(commands.Cog, name='Pong Cog'):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        await ctx.send('Pong')
+
+    @commands.command()
+    async def pong(self, ctx: commands.Context):
+        await ctx.send('Ping')
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Pong(bot))
