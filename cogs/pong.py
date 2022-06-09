71af190b6ade048e7d062be753872372311d9481
Matty <matty258987@gmail.com>
2022-06-09 01:30:51 +0100

    random and admin

--- cogs/pong.py
+++ cogs/pong.py
@@ -5,11 +5,11 @@ class Pong(commands.Cog, name='Pong Cog'):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(name='Ping', hidden=False)
     async def ping(self, ctx: commands.Context):
         await ctx.send('Pong')
 
-    @commands.command()
+    @commands.command(name='Pong', hidden=False)
     async def pong(self, ctx: commands.Context):
         await ctx.send('Ping')
 
